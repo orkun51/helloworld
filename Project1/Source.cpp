b38f3753a8b3b2b3245107cdeea3f5d15e441ee5
Orkun Koçak <orkunkocak7@gmail.com>
2022-12-21 14:11:37 +0300

    updatedv2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -89,6 +89,13 @@ int main()
 		<	Less than	x < y
 		>=	Greater than or equal to	x >= y
 		<=	Less than or equal to	x <= y
+
+		################################################
+
+		&& 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10
+		|| 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4		
+		!	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
 	*/
 	
 
